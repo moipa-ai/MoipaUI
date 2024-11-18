6591738660250bd6e42300f4fe39b7a9fc40595b
moipa <moipa_77@hotmail.com>
2024-11-18 23:46:42 +0800

    添加json转换

--- MoipaUI/Models/MqttModel.cs
+++ MoipaUI/Models/MqttModel.cs
@@ -1,11 +1,16 @@
 using MoipaUI.IServers;
 using NATS.Net;
+using Newtonsoft.Json;
 
 namespace MoipaUI.Models;
 
 public class MqttModel:IMqttServer
 {
     private NatsClient NatsClient { get; set; } = new NatsClient("192.168.3.9:4222");
+    
+    
+    [JsonProperty("msg")]
+    public string? Msg{get;set;}
 
     public MqttModel()
     {
@@ -14,6 +19,15 @@ public class MqttModel:IMqttServer
 
     public async Task PublishMessage(string message)
     {
-      await  NatsClient.PublishAsync("/.test.topic2", message);
+        var sMsg = new MqttModel { Msg = message };
+        var msg = JsonConvert.SerializeObject(sMsg);
+        //string json1 = JsonSerializer.Serialize();
+      await  NatsClient.PublishAsync("/.test.topic2", msg);
     }
+
+    // private string StringToJson(string message)
+    // {
+    //     var jsonText =string.Format($"{{ \"msg\":{message}}}"); 
+    //     return jsonText;
+    // }
 }
